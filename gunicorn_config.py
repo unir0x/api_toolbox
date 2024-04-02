7263ec2a619163b4c452dfc668f3d53a396cf2e3
unir0x <150746459+unir0x@users.noreply.github.com>
2024-04-02 15:56:52 +0200

    Update gunicorn_config.py
    Logg rotating

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -12,11 +12,39 @@
 # critical: Loggar endast de meddelanden som indikerar allvarliga problem som kan påverka applikationens tillgänglighet eller stabilitet.
 
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 
-workers = int(os.getenv('GUNICORN_WORKERS', 5))  # Använder miljövariabeln eller standardvärdet 5 om variabeln inte finns
-bind = '0.0.0.0:8000'
-timeout = int(os.getenv('GUNICORN_TIMEOUT', 120))
-worker_class = 'gevent'
+#workers = int(os.getenv('GUNICORN_WORKERS', 5))  # Använder miljövariabeln eller standardvärdet 5 om variabeln inte finns
+#bind = '0.0.0.0:8000'
+#timeout = int(os.getenv('GUNICORN_TIMEOUT', 120))
+#worker_class = 'gevent'
+#loglevel = os.getenv('GUNICORN_LOG_LEVEL', 'info')
+#accesslog = '/app/logs/access.log'  # Access loggfilens sökväg
+#errorlog = '/app/logs/error.log'  # Error loggfilens sökväg
+
+def ensure_logfile_directory_exists(path):
+    # Säkerställer att mappen för loggfilen finns. Skapar mappen om nödvändigt.
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+def custom_rotating_file_handler(path, maxBytes=10485760, backupCount=5):
+    # Skapar en anpassad RotatingFileHandler.
+    # maxBytes: Maximal storlek för en loggfil i bytes, standard är 10MB.
+    # backupCount: Antalet backup-filer att behålla, standard är 5.
+    ensure_logfile_directory_exists(path)
+    return RotatingFileHandler(path, maxBytes=maxBytes, backupCount=backupCount)
+
+# Loggkonfiguration
 loglevel = os.getenv('GUNICORN_LOG_LEVEL', 'info')
-accesslog = '/app/logs/access.log'  # Access loggfilens sökväg
-errorlog = '/app/logs/error.log'  # Error loggfilens sökväg
+accesslog_path = '/app/logs/access.log'
+errorlog_path = '/app/logs/error.log'
+
+# Använd anpassade RotatingFileHandlers för access och error loggar
+access_log_handler = custom_rotating_file_handler(accesslog_path)
+error_log_handler = custom_rotating_file_handler(errorlog_path)
+
+# Övriga Gunicorn-inställningar
+workers = int(os.getenv('GUNICORN_WORKERS', 5))
+bind = '0.0.0.0:8000'
+timeout = int(os.getenv('GUNICORN_TIMEOUT', 20))
+worker_class = 'gevent'
